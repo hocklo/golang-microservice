d887e863a514bd6a266fe9275e2157d33620519e
Eduard Coll <ecollvila@gmail.com>
2016-10-14 20:29:21 +0200

    Added time import and use the time.Now() function

--- microservice.go
+++ microservice.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"encoding/json"
+	"time"
 )
 
 const (
@@ -11,10 +12,11 @@ const (
 )
 
 type Message struct {
-	Text	string
-	Version	string
-	Date	string
-	Author	string
+	Text		string
+	Version		string
+	ZuluDate	string
+	ParsedDate	string
+	Author		string
 }
 
 /**
@@ -29,8 +31,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
  * Output a message about this go program.
  */
 func about(w http.ResponseWriter, r *http.Request) {
+	zuluDate:= time.Now().Format(time.RFC3339)
+	parsedDate:= time.Now().Format("02-01-2006")
 	// Save the message inside m
-	m:= Message{"Welcome to the Hocklo API","0.1,","2016/10/12", AUTHOR}
+	m:= Message{"Welcome to the Hocklo API","0.1,", zuluDate, parsedDate, AUTHOR}
 	// Marshal "m" inside "b" if some problem occurs the value are writed inside "err".
 	b, err := json.Marshal(m)
 	// Check the value of "err" if it isn't "nil" output a panic error.
