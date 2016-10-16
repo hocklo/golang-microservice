e45449be006aa2239e792a0b26b3bf52ca37dd4a
Eduard Coll <ecollvila@gmail.com>
2016-10-16 12:50:49 +0200

    Added log info at start and inside of about function

--- microservice.go
+++ microservice.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"encoding/json"
 	"time"
+	"log"
 )
 
 const (
@@ -31,6 +33,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
  * Output a message about this go program.
  */
 func about(w http.ResponseWriter, r *http.Request) {
+	logInfo("func::about::start::"+time.Now().Format(time.RFC3339))
 	zuluDate:= time.Now().Format(time.RFC3339)
 	parsedDate:= time.Now().Format("02-01-2006")
 	// Save the message inside m
@@ -41,6 +44,8 @@ func about(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+
+	logInfo("func::about::end::"+time.Now().Format(time.RFC3339))
 	// Write the value of "b" at ResponseWriter
 	w.Write(b)
 }
@@ -49,7 +54,20 @@ func about(w http.ResponseWriter, r *http.Request) {
  * Main function to handle all mappings
  */
 func main() {
+	logInfo("GoMicroservice! Started!")
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/about", about)
 	http.ListenAndServe(":8080", nil)
 }
+
+
+/**
+ * Print &buf to log.
+ */
+func logInfo(s string) {
+	var buf bytes.Buffer // Instance buffer
+	var logger = log.New(&buf, "logger: ", log.Lshortfile) // Instance log 
+	logger.Print(s) // Print log to buffer
+	fmt.Print(&buf) // throw log from buffer to file.
+}
+
