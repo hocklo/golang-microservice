4652ee4d9e245f31ed2792e80185ad15ddaeea87
Eduard Coll <ecollvila@gmail.com>
2016-10-22 11:39:59 +0200

    Added an error management to output panic errors and ERROR logs

--- microservice.go
+++ microservice.go
@@ -10,6 +10,7 @@ import (
 
 const (
 	AUTHOR = "hocklo"
+	HTTP_PORT = ":8080"
 )
 
 type Message struct {
@@ -42,9 +43,7 @@ func about(w http.ResponseWriter, r *http.Request) {
 	// Marshal "m" inside "b" if some problem occurs the value are writed inside "err".
 	b, err := json.Marshal(m)
 	// Check the value of "err" if it isn't "nil" output a panic error.
-	if err != nil {
-		panic(err)
-	}
+	errorManagement(err, "func::about::Json marshall of message failed!")
 	// Write the value of "b" at ResponseWriter
 	w.Write(b)
         logger.Audit("func::about::end")
@@ -57,10 +56,24 @@ func main() {
 	logger.Info("func::main::GoMicroservice! Started!")
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/about", about)
-	err:= http.ListenAndServe(":8080", nil)
-	if err != nil {
-		logger.Error("func::main::GoMicroservice! BOOM!!")
-		panic(err)
-	}
+	err:= http.ListenAndServe(HTTP_PORT, nil)
+	// Check the value of "err" if it isn't nil output a panic error
+	//errorManagement(err, "func::main::GoMicroservice! BOOM!!")
+	//errorManagement(err)
+	errorManagement(err, "func::main::GoMicroservice! BOOM!!","Do commit before run", "Your server is down.")
 }
 
+
+/**
+ * Error management
+ * Params err Error to throw, m Message to log with ERROR lvl.
+ */
+func errorManagement(err error, messages ...string) {
+	// Check the value of "err" if it isn't nil output a panic error
+	if err != nil {
+		for i := 0; i< len(messages); i++ {
+			logger.Error(messages[i])
+		}
+                panic(err)
+        }
+}
