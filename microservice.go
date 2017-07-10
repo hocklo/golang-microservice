2fafaaff78ffaa5714c80add206bc660870b9237
Eduard Coll <ecollvila@gmail.com>
2017-07-10 23:04:40 +0200

    Modified microservice.go

--- microservice.go
+++ microservice.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+        "html"
 	"encoding/json"
+	"runtime"
 	"time"
 	"github.com/hocklo/logger"
 )
@@ -11,6 +13,8 @@ import (
 const (
 	AUTHOR = "hocklo"
 	HTTP_PORT = ":8080"
+	START = "start"
+	END = "end"
 )
 
 type Message struct {
@@ -26,16 +30,16 @@ type Message struct {
  * e.g : Welcome, hocklo!
  */
 func handler(w http.ResponseWriter, r *http.Request) {
-	logger.Audit("func::handler::start")
+	audit(START)
 	// localhost:8080/{argument} 
-	fmt.Fprintf(w, "Welcome, %s!", r.URL.Path[1:])
-	logger.Audit("func::handler::end")
+	fmt.Fprintf(w, "Welcome, %q!", html.EscapeString(r.URL.Path[1:]))
+	audit(END)
 }
 /**
  * Output a message about this go program.
  */
 func about(w http.ResponseWriter, r *http.Request) {
-	logger.Audit("func::about::start")
+	audit(START)
 	zuluDate:= time.Now().Format(time.RFC3339)
 	parsedDate:= time.Now().Format("02-01-2006")
 	// Save the message inside m
@@ -45,14 +49,16 @@ func about(w http.ResponseWriter, r *http.Request) {
 	// Check the value of "err" if it isn't "nil" output a panic error.
 	errorManagement(err, "func::about::Json marshall of message failed!")
 	// Write the value of "b" at ResponseWriter
-	w.Write(b)
-        logger.Audit("func::about::end")
+	//w.Write(b)
+        fmt.Fprintf(w, "%s", b)
+        audit(END)
 }
 
 /**
  * Main function to handle all mappings
  */
 func main() {
+	audit(START)
 	logger.Info("func::main::GoMicroservice! Started!")
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/about", about)
@@ -61,6 +67,7 @@ func main() {
 	//errorManagement(err, "func::main::GoMicroservice! BOOM!!")
 	//errorManagement(err)
 	errorManagement(err, "func::main::GoMicroservice! BOOM!!","Do commit before run", "Your server is down.")
+	audit(END)
 }
 
 
@@ -76,4 +83,24 @@ func errorManagement(err error, messages ...string) {
 		}
                 panic(err)
         }
+}
+
+/**
+ * Return the name of func
+ */
+func this() *runtime.Func {
+        pc := make([]uintptr, 10)  // at least 1 entry needed
+        runtime.Callers(2, pc)
+        f:= runtime.FuncForPC(pc[1])
+        return f
+}
+
+func audit(m string) {
+	// 
+	switch m {
+	case START:
+		logger.Audit("func::"+this().Name()+"::"+START)
+	case END:
+		logger.Audit("func::"+this().Name()+"::"+END)
+	}
 }
